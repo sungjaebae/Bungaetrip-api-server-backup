9c14b305c0ab0e576e977501c73df473072dabec
rlagudtn <rlagudtn4510@naver.com>
2022-06-27 20:33:37 +0900

    feat : Start Go Getter develop

---
+++ src/main/java/GoGetters/GoGetter/GoGetterApplication.java
@@ -0,0 +1,13 @@
+package GoGetters.GoGetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoGetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoGetterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GoGetters/GoGetter/GoGetterApplicationTests.java
@@ -0,0 +1,13 @@
+package GoGetters.GoGetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoGetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
