6b4c3b8c9314fe999cf82da8a26e38c4c592be3f
rlagudtn <rlagudtn4510@naver.com>
2022-07-11 17:22:31 +0900

    feat : Upload image file to s3

--- src/main/java/GoGetters/GoGetter/GoGetterApplication.java
+++ src/main/java/GoGetters/GoGetter/GoGetterApplication.java
@@ -2,12 +2,18 @@ package GoGetters.GoGetter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class GoGetterApplication {
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "classpath:aws.yml";
 
 	public static void main(String[] args) {
-		SpringApplication.run(GoGetterApplication.class, args);
+		new SpringApplicationBuilder(GoGetterApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+						.run(args);
 	}
 
 }
