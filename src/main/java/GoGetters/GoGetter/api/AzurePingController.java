860f7310ac19ba6f2999b207270d7eed72b207a3
rlagudtn <rlagudtn4510@naver.com>
2022-07-31 10:07:48 +0900

    feat : Azure App Service 에서 지속적으로 앱을 실행시키기 위해 ping 추가

---
+++ src/main/java/GoGetters/GoGetter/api/AzurePingController.java
@@ -0,0 +1,19 @@
+package GoGetters.GoGetter.api;
+
+import GoGetters.GoGetter.util.ResponseUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class AzurePingController {
+
+    @GetMapping("/ping")
+    public ResponseEntity ping(){
+        return ResponseUtil.successResponse(HttpStatus.OK,"pong");
+    }
+}
